6d35f0c5b896ebda8f4de9cf2b25cbbe3fcc0714
Rohit Waghchaure <rohitw1991@gmail.com>
2021-03-07 11:58:13 +0530

    fix: not able to collapse section in modal using code

--- frappe/public/js/frappe/form/layout.js
+++ frappe/public/js/frappe/form/layout.js
@@ -283,7 +283,7 @@ frappe.ui.form.Layout = Class.extend({
 	},
 
 	refresh_section_collapse: function () {
-		if (!this.doc) return;
+		if (!(this.sections && this.sections.length)) return;
 
 		for (var i = 0; i < this.sections.length; i++) {
 			var section = this.sections[i];
